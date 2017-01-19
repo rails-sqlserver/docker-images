b68dd8a836508b78c43924096c59fd6929c8436a
Ken Collins <ken@metaskills.net>
2017-01-18 19:06:27 -0500

    v1.1 - mssql-server-linux-tinytds

--- mssql-server-linux-tinytds/README.md
+++ mssql-server-linux-tinytds/README.md
@@ -9,24 +9,35 @@ Using the official image for Microsoft SQL Server for Linux.
 
 Includes test database & users w/permissions needed to test TinyTDS.
 
+All commands assume that the `$PWD` is this directory.
 
-#### Building
 
-Assuming that the `$PWD` is this directory.
+### Build
+
+Fully automated build
+
+```shell
+./build.sh
+```
+
+
+### Build Testing
+
+Simple build command.
 
 ```shell
 docker build -t metaskills/mssql-server-linux-tinytds .
 ```
 
-#### Verify
-
 Ensure that the build script worked.
 
 ```shell
-container = $(docker ps -a -q --filter ancestor=metaskills/mssql-server-linux-rails)
+docker run -p 1433:1433 -d metaskills/mssql-server-linux-tinytds
+container=$(docker ps --all --quiet --filter ancestor=metaskills/mssql-server-linux-tinytds)
 docker exec -it $container bash
 
 sqsh -U sa -P super01S3cUr3 -S localhost -C "SELECT name FROM master.dbo.sysdatabases"
 sqsh -U sa -P super01S3cUr3 -S localhost -C "SELECT loginname, dbname FROM syslogins"
-tsql -H localhost -p 1433 -U tinytds -P '' -D tinytdstest
+
+docker stop $container
 ```
--- mssql-server-linux-tinytds/build.sh
+++ mssql-server-linux-tinytds/build.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-image=$(docker ps --all --quiet --filter ancestor=metaskills/mssql-server-linux-rails)
+image=$(docker ps --all --quiet --filter ancestor=metaskills/mssql-server-linux-tinytds)
 if [[ -n $image ]]; then
+  docker stop $image
   docker rmi -f $image
 fi
 
+images=$(docker images --all --quiet metaskills/mssql-server-linux-tinytds)
+if [[ -n $images ]]; then
+  docker rmi -f $images
+fi
+
+images=$(docker images --all --quiet microsoft/mssql-server-linux)
+if [[ -n $images ]]; then
+  docker rmi -f $images
+fi
+
+docker pull microsoft/mssql-server-linux:latest
 docker build --no-cache -t metaskills/mssql-server-linux-tinytds .
 
 container=$(docker run -p 1433:1433 -d metaskills/mssql-server-linux-tinytds)
 sleep 12
 docker exec $container /opt/tinytds/setup.sh
 docker commit $container metaskills/mssql-server-linux-tinytds
+docker tag metaskills/mssql-server-linux-tinytds metaskills/mssql-server-linux-tinytds:1.1
+docker push metaskills/mssql-server-linux-tinytds:1.1
 docker push metaskills/mssql-server-linux-tinytds
 docker stop $container
